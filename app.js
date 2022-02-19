68b8a51c45315ecfe3a048e5b3687f2aca7d84eb
sammydowds <sammydowds1993@gmail.com>
2022-02-19 14:59:06 -0600

    feat: nodemailer for sending emails

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const schedule = require('node-schedule');
+const schedule = require('node-schedule')
 const http = require('https')
+const nodemailer = require('nodemailer')
 require('dotenv').config();
 
 const FINANCE_KEY = process.env.YHOO_KEY
 
-const options = {
+const yahooApiOptions = {
 	"method": "GET",
 	"hostname": "yfapi.net",
 	"port": null,
@@ -14,22 +15,46 @@ const options = {
 	}
 };
 
-schedule.scheduleJob('5 8 * * *', () => {
-  console.log('Fetching data -------------------')
-  const req = http.request(options, function (res) {
-	const chunks = [];
-
-	res.on("data", function (chunk) {
-		chunks.push(chunk);
-	});
-
-	res.on("end", function () {
-		const body = Buffer.concat(chunks);
-		console.log(body.toString());
-	});
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PWORD,
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN
+  }
 });
 
-req.end();
+schedule.scheduleJob('16 12 * * *', () => {
+  console.log('Fetching data -------------------')
+  const req = http.request(yahooApiOptions, function (res) {
+    const chunks = [];
+
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on("end", function () {
+      const body = Buffer.concat(chunks);
+      console.log(body.toString());
+      let mailOptions = {
+        from: process.env.FROM_EMAIL,
+        to: process.env.TO_EMAIL,
+        subject: 'Daily Brief',
+        text: body.toString()
+      };
+      transporter.sendMail(mailOptions, function(err, data) {
+        if (err) {
+          console.log("Error " + err);
+        } else {
+          console.log("Email sent successfully");
+        }
+      });
+    });
+  })
+  req.end();
   console.log('Job done ------------------')
 })
 
